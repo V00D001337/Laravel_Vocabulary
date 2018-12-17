81c82222578cc5599b3dd32d4e5aa5dbb57f56c7
V00D001337 <kosowski.kar@gmail.com>
2018-12-17 20:55:16 +0100

    widok dodaj kategoria
    jak w temacie

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,9 @@ Auth::routes();
 Route::get('/home', 'CategoriesController@index')->name('home');
 
 Route::get('/category/{categoryId}', 'SubcategoriesController@index');
-Route::get('/category/create', 'CategoriesController@create');
+
+Route::get('/categories/create', 'CategoriesController@create');
+Route::get('/categories/store', 'CategoriesController@store');
 
 Route::get('/pieczywo', 'PieczywoController@index');
 Route::get('/pieczywo/create', 'PieczywoController@create');
