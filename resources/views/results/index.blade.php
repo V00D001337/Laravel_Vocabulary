6ea2d03b2da3b61e86164ae26b5cea2321aa4e9f
V00D001337 <kosowski.kar@gmail.com>
2019-01-03 22:05:59 +0100

    lel

--- resources/views/results/index.blade.php
+++ resources/views/results/index.blade.php
@@ -4,4 +4,6 @@
 {!! $chart->container() !!}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.js"></script>
 {!! $chart->script() !!}
+{!! $chart1->container() !!}
+{!! $chart1->script() !!}
 @endsection
