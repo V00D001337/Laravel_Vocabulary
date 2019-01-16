9e35a30d13431d413b452cc8808e99839649b716
V00D001337 <kosowski.kar@gmail.com>
2019-01-16 18:02:49 +0100

    fail

--- database/migrations/2019_01_16_165252_create_users_subcategories_table.php
+++ database/migrations/2019_01_16_165252_create_users_subcategories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersSubcategories extends Migration
+class CreateUsersSubcategoriesTable extends Migration
 {
     /**
      * Run the migrations.
