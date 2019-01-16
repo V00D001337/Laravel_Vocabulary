ef2e767b5e756e0b8e19236b5c30dc2f9d47362f
V00D001337 <kosowski.kar@gmail.com>
2019-01-16 17:38:13 +0100

    userController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+use DateTime;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('users.index', ['users' => $users]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $users = new User;
+        $users->name = $request->validate(['name' => 'required|max:100']);
+        $users->email = $request->validate(['email' => 'required|regex:^([\w\-\.]+)@((\[([0-9]{1,3}\.){3}[0-9]{1,3}\])|(([\w\-]+\.)+)([a-zA-Z]{2,4}))$']);
+        $users->password = $request->validate(['password' => 'required|max:191']);
+        $users->name = $request->input('name');
+        $users->name = $request->input('email');
+        $users->name = $request->input('password');
+        $users->email_verified_at = new DateTime();
+        $users->save();
+            return redirect()->action('UserController@index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $users = User::find($id);
+        return view('users.update', compact('users', 'id'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $users = User::find($id);
+        $users->name = $request->validate(['name' => 'required|max:100']);
+        $users->email = $request->validate(['email' => 'required|regex:^([\w\-\.]+)@((\[([0-9]{1,3}\.){3}[0-9]{1,3}\])|(([\w\-]+\.)+)([a-zA-Z]{2,4}))$']);
+        $users->password = $request->validate(['password' => 'required|max:191']);
+        $users->name = $request->input('name');
+        $users->name = $request->input('email');
+        $users->name = $request->input('password');
+        $users->email_verified_at = new DateTime();
+        $users->save();
+            return redirect()->action('UserController@index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
